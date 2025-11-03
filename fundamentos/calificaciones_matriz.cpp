63503c264b3f62ee450dc0b9f2ec69798d8eec28
favian <favianorduna@gmail.com>
2025-11-03 09:23:45 -0600

    Calificaciones matricez

---
+++ fundamentos/calificaciones_matriz.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+main()
+{
+    int i,j,n;
+    printf("Ingrese cantidad de alumnos: ");
+    scanf("%i",&n);
+    if(n>0 && n<=30){
+    	int C[n][4];
+		float P[n];
+        for(i=0;i<n;i++){
+            P[i]=0;
+        }
+        for(i=0;i<n;i++){
+            for(j=0;j<4;j++){
+                do{
+                    printf("Ingrese calificacion %i del alumno %i: ",j+1,i+1);
+                    scanf("%i",&C[i][j]);
+                    if(C[i][j]>=0 && C[i][j]<=100){
+                        P[i]=P[i]+C[i][j];
+                        if(j==3){
+                        	P[i]=P[i]/4;
+						}
+                    }else{
+                        printf("Ingrese una calificacion valida 0-100\n");
+                    }
+                }while(C[i][j]>100 || C[i][j]<0);
+            }
+        }
+        for(i=0;i<n;i++){
+            printf("\nPromedio del alumno %i: %.2f",i+1,P[i]);
+        }
+    }else{
+        printf("Ingrese una cantidad de alumnos valida");
+    }
+}
+
